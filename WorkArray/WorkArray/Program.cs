457c40d6a196a3ebc33fc0a673f1b31428270220
Andramonova92 <bakaeva92@list.ru>
2016-02-06 23:32:46 +0200

    WorkArray method 2 ListOrArraylist 6.02.2016

--- WorkArray/WorkArray/Program.cs
+++ WorkArray/WorkArray/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace WorkArray
 {
@@ -10,55 +11,50 @@ namespace WorkArray
     {
         static void Main(string[] args)
         {
-            arrayTable();
+            int count=10;
+            ListOrArraylist(count);
         }
 
-        static void arrayTable()
+
+        public static void ListOrArraylist(int count)
         {
-            // Объявляем двумерный массив
-            int[,] myArr = new int[11, 11];
-            //задаем значения первым множителям(строкам)
-            myArr.SetValue(1, 0, 1); //первое число значение, второе номер строки, третье номер столбца
-            myArr.SetValue(2,1,1);
-            myArr.SetValue(3,2,1);
-            myArr.SetValue(4,3,1);
-            myArr.SetValue(5,4,1);
-            myArr.SetValue(6,5,1);
-            myArr.SetValue(7,6,1);
-            myArr.SetValue(8,7,1);
-            myArr.SetValue(9,8,1);
-            myArr.SetValue(10,9,1);
-            //задаем значение вторым множителям(столбцам)
-            myArr.SetValue(1,1,0);//первое число значение, второе номер столбца, третье номер строки
-            myArr.SetValue(2,1,1);
-            myArr.SetValue(3,1,2);
-            myArr.SetValue(4,1,3);
-            myArr.SetValue(5,1,4);
-            myArr.SetValue(6,1,5);
-            myArr.SetValue(7,1,6);
-            myArr.SetValue(8,1,7);
-            myArr.SetValue(9,1,8);
-            myArr.SetValue(10,1,9);
+            string elements;
+            List<int> list = new List<int>();
+            Console.WriteLine("Enter elements");
+            elements = Console.ReadLine();
+            string[] el = elements.Split(' ');
+
             
-            // циклы для выполнения умножения
-            for (int i = 1; i <=10; i++)
+            if (el.Length <= count)
             {
-                for (int j = 1; j <= 10; j++)
+                for (int i = 0; i < count; i++)
                 {
-                    myArr[i, j] = j*i;
-                    
-                    if (i == j) { Console.ForegroundColor = ConsoleColor.Blue; }
-                    else { Console.ForegroundColor = ConsoleColor.White; }
-                    Console.Write("{0}\t", myArr[i, j]);//вывод массива
+                    int m = int.Parse(el[i]);
+                    list.Insert(i, m);
                 }
-                Console.WriteLine();
+                foreach (int l in list)
+                    Console.WriteLine("list {0}", l);
+                Console.ReadKey();
             }
 
-            
-           
-            
-            Console.ReadLine();
-           }
+
+            else
+            {
+                ArrayList arraylist = new ArrayList();
+                for (int i = 0; i < el.Length; i++)
+                {
+                    int m = int.Parse(el[i]);
+                    arraylist.Insert(i, m);
+
+                }
+                foreach (int a in arraylist)
+                    Console.WriteLine("arraylist {0}", a);
+                Console.ReadKey();
+            }  
+            }
         }
-}
+    }
+
+    
+
  
