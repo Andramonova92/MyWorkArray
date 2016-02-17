be6ae01626a1070f7db1f8f7b7d3d957b260c640
Andramonova92 <bakaeva92@list.ru>
2016-02-17 23:39:54 +0200

    method 2 in WorkArray.It's work) 17.02.2016

--- List_Arraylist/List_Arraylist/Program.cs
+++ List_Arraylist/List_Arraylist/Program.cs
@@ -15,38 +15,45 @@ namespace List_Arraylist
             List<int> List = new List<int>();
             int[] myInts = new int[25];
             ListOrArraylist(Arraylist, List, myInts);
+            Console.WriteLine("Your enter");
+            Console.ReadLine();
+            foreach (int l in List)
+                Console.WriteLine("list {0}", l);
+            foreach(int a in Arraylist)
+            Console.WriteLine("arraylist {0}", a);
+            Console.WriteLine("Arraylist count {0}",Arraylist.Count);
+            Console.ReadKey();
         }
 
      
         static void ListOrArraylist(ArrayList arraylist, List<int> list,int[] myints) 
         {
           Random ran = new Random();
-        for (int i = 0; i < 10; i++)
-        {
-        if (i<10)
-        {
-            myints[i] = ran.Next(1,25);
-            list.Add(myints[i]);
+          for (int i = 0; i < myints.Length; i++)
+          {
+              myints[i] = ran.Next(1, 25);
+              if (i < 10)
+              {
+
+                  list.Add(myints[i]);
+              }
+          }
+              
+                  arraylist.AddRange(list);
+                  FillArrayList(list,arraylist, myints);
+                              
         }
-        arraylist.AddRange(list);
-        FillArrayList(i, arraylist, myints);
-}
-        Console.WriteLine("Your enter");
-        Console.ReadLine();
-            foreach(int l in list)
-        Console.WriteLine("list {0}",l);
-           Console.WriteLine("arraylist {0}", arraylist.Count);
-            Console.ReadKey();
-                
-        }
-       static void FillArrayList(int i,ArrayList arraylist,int[] myints) 
+       static void FillArrayList(List<int>list,ArrayList arraylist,int[] myints) 
         {
-            for (int j = i; i < myints.Length; i++)
+            Random ran = new Random();
+
+            for (int j = list.Count; j < myints.Length; j++)
             {
-                
-                arraylist.Add(myints[i]);
+                myints[j] = ran.Next(1, 25);
+                arraylist.Add(myints[j]);
                 
             }
+
            
         }
     }
